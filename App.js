1a91db2b37e30f84e99cd1eb3bd4daa4ac6e78ca
Your Name <you@example.com>
2025-05-02 23:02:25 +0200

    Image, local, URI and ImageBackground

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, ImageBackground } from "react-native";
+const logoImg = require("./assets/splash-icon.png"); //take pictre locally
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 60, flex: 1, backgroundColor: "plum" }}>
+      {/* <Text style={{ backgroundColor: "grey" }}>One</Text>
+      <Image style={{ height: 300, width: 250 }} source={logoImg}></Image>
+      <Image
+        style={{ height: 30, width: 25 }}
+        source={{ uri: "https://picsum.com/photos/300" }}
+      ></Image> */}
+      <ImageBackground style={{ flex: 1, }} source={logoImg}>
+        <Text >TEXT HERE </Text>
+      </ImageBackground>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
